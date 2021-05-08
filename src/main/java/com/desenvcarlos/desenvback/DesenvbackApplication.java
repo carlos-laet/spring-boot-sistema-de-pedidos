99f5ddcd519760dfece563b83cea2ff7074e5725
carlos-laet <carlosedulaet@hotmail.com>
2021-05-08 14:55:39 -0300

    Projeto criado

---
+++ src/main/java/com/desenvcarlos/desenvback/DesenvbackApplication.java
@@ -0,0 +1,13 @@
+package com.desenvcarlos.desenvback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desenvcarlos/desenvback/DesenvbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvcarlos.desenvback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

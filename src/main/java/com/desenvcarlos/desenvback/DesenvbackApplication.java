4930734db503a77f5c2af49190df07fbd8431e92
carlos-laet <carlosedulaet@hotmail.com>
2021-06-09 16:58:12 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/desenvcarlos/desenvback/DesenvbackApplication.java
+++ src/main/java/com/desenvcarlos/desenvback/DesenvbackApplication.java
@@ -1,12 +1,17 @@
 package com.desenvcarlos.desenvback;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.desenvcarlos.desenvback.services.S3Service;
+
 @SpringBootApplication
 public class DesenvbackApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 
 	
 	public static void main(String[] args) {
@@ -15,6 +20,7 @@ public class DesenvbackApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\Laet\\Pictures\\Camera Roll\\transferir.jpg");
 	}
 
 }

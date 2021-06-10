71714d8e64e4399000fafa7f8ebc436ad5e6a2dc
carlos-laet <carlosedulaet@hotmail.com>
2021-06-10 16:43:20 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/desenvcarlos/desenvback/domain/Cliente.java
+++ src/main/java/com/desenvcarlos/desenvback/domain/Cliente.java
@@ -55,7 +55,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -152,15 +151,7 @@ public class Cliente implements Serializable {
 	public void addPerfil(Perfil perfil) {
 		perfis.add(perfil.getCod());
 	}
- 	
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+ 
 
 	@Override
 	public int hashCode() {

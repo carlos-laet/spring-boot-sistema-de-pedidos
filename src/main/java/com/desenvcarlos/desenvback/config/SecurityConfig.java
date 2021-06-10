301a2cf7f7984ca211f116593bbd1b1465f934e4
carlos-laet <carlosedulaet@hotmail.com>
2021-06-10 20:47:23 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/desenvcarlos/desenvback/config/SecurityConfig.java
+++ src/main/java/com/desenvcarlos/desenvback/config/SecurityConfig.java
@@ -43,8 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	public static final String[] PUBLIC_MATCHERS_GET = {
-			"/produtos/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/produtos/**"
 	};
 	
 	public static final String[] PUBLIC_MATCHERS_POST = {
